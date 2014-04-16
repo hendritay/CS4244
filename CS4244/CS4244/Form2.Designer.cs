df555b2232eb0009333f76e76e4f52059715c612
aaronwjw <aaronwjw89@gmail.com>
2014-04-16 15:39:58 +0800

    Edited FocusArea

--- CS4244/CS4244/Form2.Designer.cs
+++ CS4244/CS4244/Form2.Designer.cs
@@ -28,24 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
-            // panel1
+            // webBrowser1
             // 
-            this.panel1.BackgroundImage = global::CS4244.Properties.Resources.homer;
-            this.panel1.Location = new System.Drawing.Point(12, 9);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(387, 459);
-            this.panel1.TabIndex = 0;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1059, 632);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://nusmods.com/2013-2014/sem2/v1/#MA2213=1B01&CS4244=81&CS2102=87&CS5232=21&M" +
+                    "A2213=8T01&MA2213=2SL1&ST2334=8T1&ST2334=2SL2&CS4244=21&CS2102=21&MA2213=2SL1&ST" +
+                    "2334=2SL2", System.UriKind.Absolute);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(425, 474);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1059, 632);
+            this.Controls.Add(this.webBrowser1);
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NUS Undergraduate Curriculum Planner";
             this.ResumeLayout(false);
 
@@ -53,7 +58,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+
 
     }
 }
